d28ce9925c4afdc91b1ed30152d3b518ffe5338c
윤정인 <yoonjeongin@naver.com>
2021-09-16 20:28:43 +0900

    세영 과제

--- etc/seyoung.py
+++ etc/seyoung.py
@@ -3,11 +3,11 @@
 #Sample Dictionary ( n = 5) :
 #Expected Output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
 
-#n 입력 받기
 #key 1~n
 #value 1~n*n
-n = int(input())
-dic = {}
-for i in range(1,n+1):
-    dic[i] = i*i
-print(dic)
+n = int(input()) #n 입력 받기
+dic = {} #빈 dictionary 생성
+for i in range(1,n+1): #i가 1~n일 때
+    dic[i] = i*i #key: i, value: i*i
+print(dic) #출력
+
