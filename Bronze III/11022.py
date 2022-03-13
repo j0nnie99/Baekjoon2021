8a44ffe697373bffa74660b8a7207b375a9a7458
yoonjeongin <yoonjeongin@naver.com>
2022-03-13 22:46:22 +0900

    단계별로 플어보기: for문

---
+++ Bronze III/11022.py
@@ -0,0 +1,7 @@
+import sys  
+
+t = int(sys.stdin.readline())
+
+for i in range(t):
+    a,b = map(int, sys.stdin.readline().split())
+    print("Case #"+str(i+1)+": "+str(a)+" + "+str(b)+" =", a+b)
